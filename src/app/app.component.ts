70d62bcf3a7cbab558511dff764caeae3a200334
Jon Zgoda <jon@zgoda.us>
2022-04-22 08:27:18 -0500

    Initial functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ns-app',
-  templateUrl: './app.component.html',
+  selector: "ns-app",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+  show: boolean = false;
+
+  tap(): void {
+    this.show = !this.show;
+  }
+}
