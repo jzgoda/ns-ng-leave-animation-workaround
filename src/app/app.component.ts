abdcb251ad2d8c8f415bad7c9bc267a95cc52c09
Jon Zgoda <jon@zgoda.us>
2022-04-22 09:30:54 -0500

    Switch to `@stateFade`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "ns-app",
   templateUrl: "./app.component.html",
+  animations: [
+    trigger("stateFade", [
+      state("show", style({ opacity: 1 })),
+      state("hide", style({ opacity: 0 })),
+      transition("show => hide", [animate("250ms", style({ opacity: 0 }))]),
+      transition("hide => show", [animate("250ms", style({ opacity: 1 }))]),
+    ]),
+  ],
 })
 export class AppComponent {
   show: boolean = false;
